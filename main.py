222bfe27573b70962ec5611c939a6e8d25381541
Ty <168314093+tyreseGill@users.noreply.github.com>
2025-11-26 23:55:22 -0800

    feat: setup CLI & error handling

---
+++ main.py
@@ -0,0 +1,151 @@
+import argparse
+import sys
+import networkx as nx
+import pandas as pd
+from utils import file_exists, has_file_extension, file_empty, remove_trailing_digits, validate_color, visualize_graph
+
+
+class SafeArgumentParser(argparse.ArgumentParser):
+    """
+    A subclass of argparse.ArgumentParser that overrides the default error behavior.
+
+    Args:
+        argparse.ArgumentParser: Object for parsing command line strings into Python objects.
+
+    Methods:
+        error(message): Overrides the default error handler to print a custom error message
+            and terminate the program with a non-zero exit code.
+    """
+    def error(self, message: str):
+        # Override the default behavior (which prints to stderr and exits)
+        print(f"Error: {message}.")
+        sys.exit(2)
+
+
+def parse_args() -> dict:
+    """
+    Manages input parameters for the command-line interface.
+
+    Returns:
+        dict: Collection of arguments and the associated input values provided by the user.
+    """
+    parser = SafeArgumentParser()
+
+    # Defines expected arguments and behavior for CLI
+    parser.add_argument(
+        "csv_file_pathway",
+        type=str,
+        help='The path to the inputted .csv graph file.'
+    )
+    parser.add_argument(
+        "--color",
+        type=str,
+        help="The coloration of the plotted nodes."
+    )
+    parser.add_argument(
+        "--title",
+        type=str,
+        help="The name of the plot."
+    )
+    parser.add_argument(
+        "--sample_size",
+        type=int,
+        default=500,
+        help="The number of nodes to be plotted."
+    )
+
+    # Returns dictionary of the parsed arguments
+    return vars(parser.parse_args())
+
+
+def verify_compatible_file(input_file: str, file_extension: str) -> bool:
+    """
+    Checks to see if the file inputted is compatible with the program. If not, forces early return.
+
+    Args:
+        input_file: The name of the .csv file containing graph data.
+        file_extension: The extension that a file is expected to end with.
+    """
+    if not input_file:
+        print("Error: No file path provided.")
+        return False
+    if not has_file_extension(input_file, file_extension):
+        return False
+    if not file_exists(input_file):
+        return False
+    if file_empty(input_file):
+        return False
+    
+    return True
+
+
+def read_csv_data(file_path: str) -> nx.Graph:
+    """
+    Acts as gaurdrail when error occurs attempting to read file.
+
+    Args:
+        file_path: The name of the .csv file containing graph data.
+    """
+    # Perform early return if file is not compatible
+    if not verify_compatible_file(file_path, ".csv"):
+        return None
+    
+    # If program reaches this point, the .csv file provided must be valid
+    try:
+        df = pd.read_csv(file_path)
+        column_names = df.columns
+        src_tar_substr = remove_trailing_digits(column_names[0])
+        G = nx.from_pandas_edgelist(df, source=f"{src_tar_substr}1", target=f"{src_tar_substr}2", create_using=nx.Graph())
+    except Exception as e:
+        print(f'Error: An issue occurred attempting to read "{file_path}": {e}.')
+        return None
+    
+    # Perform early return if data is not compatible
+    if nx.is_directed(G):
+        print("Error: The provided .csv graph must be undirected for this simulation.")
+        return None
+    
+    if G.number_of_nodes() == 0:
+        print("Error: The graph is empty.")
+        return None
+    
+    if not G:
+        print("Error: An issue arose attempting to read the provided .csv file. It may be incomplete.")
+        return None
+    
+    return G
+
+
+def main():
+    params = parse_args()
+
+    # Extracts parsed arguments
+    csv_file = params["csv_file_pathway"]
+    color = params["color"]
+    title = params["title"]
+    sample_size = params["sample_size"]
+
+    G = None
+
+    # Check to ensure user-provided color is valid
+    if color:
+        color = validate_color(color)
+
+    if csv_file:
+        G = read_csv_data(csv_file)
+        
+        # Displays multiple communities with color-coding
+        if "facebook_large" in csv_file:
+            visualize_graph(G, title, sample_size, color_code=True)
+        # Displays single community with uniform color
+        else:
+            visualize_graph(G, title, sample_size, color=color)
+    
+    # Check to ensure .csv graph data is given
+    if not G:
+        print("Error: Unable to read .csv file.")
+        return
+
+
+if __name__ == "__main__":
+    main()
